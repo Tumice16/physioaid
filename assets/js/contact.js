53b559dbed6f094ea29a3e8f6971a153d59de5e9
Tumice16 <you@example.com>
2024-07-08 17:19:25 +0100

    Upadated files

---
+++ assets/js/contact.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Tell us your name",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                subject: {
+                    required: "Why are you sending us a message?",
+                    minlength: "your subject must consist of at least 4 characters"
+                },
+                number: {
+                    required: "Tell us your phone number",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Tell us your email"
+                },
+                message: {
+                    required: "um...yea, you have to write something to send this form.",
+                    minlength: "thats all? really?"
+                }
+            },
+        })
+    })
+        
+ })(jQuery)
+})
